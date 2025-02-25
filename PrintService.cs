35e1eb5bc55d62f92abd34074367837f6127250d
Richard Yuri <richardyuri930@gmail.com>
2025-02-25 20:05:31 -0300

    Add files via upload

---
+++ PrintService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    internal class PrintService
+    {
+        private int[] valores = new int[10];
+        public int count = 0;
+
+        public void AddValue(int valor)
+        {
+            valores[count] = valor;
+            count++;
+        }
+
+        public int First()
+        {
+            return valores[0];
+        }
+
+        public void Print()
+        {
+            Console.Write("[");
+            for (int i = 0; i < count - 1; i++)
+            {
+                Console.Write(valores[i] + ", ");
+            }
+            Console.Write(valores[count - 1] + "]");
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Quantos valores? ");
+            int n = int.Parse(Console.ReadLine());
+
+            PrintService printService = new PrintService();
+
+            for (int i = 0; i < n; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+                printService.AddValue(x);
+            }
+
+            Console.WriteLine("Primeiro elemento: " + printService.First());
+
+            Console.WriteLine("-----------");
+
+            printService.Print();
+        }
+    }
+}
